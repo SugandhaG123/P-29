70af9a855d925485609e77b65a8a69af9218a37d
SugandhaG123 <72544711+SugandhaG123@users.noreply.github.com>
2020-10-28 13:16:54 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,35 +1,25 @@
-class block{
-
-  constructor(x, y, width, height,angle) {
-    
-      var options = {
-        isStatic:false,
-        restitution:0.4,
-         friction:0
-          
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
       }
-     
-      
-      this.x=x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.body = Bodies.rectangle(x, y, width, height, options);
-    
-      World.add(world, this.body);
-    }
-    display(){
-      var angle = this.body.angle;
-      var pos =this.body.position;
-      push();
-      
-      translate(pos.x, pos.y);
-      rotate(angle);
-      
-      fill("brown");
-      rectMode(CENTER);
-      rect(0,0,this.width, this.height);
-     
-      pop();
-    }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
 }
